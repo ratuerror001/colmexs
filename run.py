3b299f39044114b97990e580a0ae8a2684b95804
JESSICA PUTRY <126640804+ratuerror001@users.noreply.github.com>
2023-03-14 21:47:36 +0700

    Update run.py

--- run.py
+++ run.py
@@ -210,7 +210,7 @@ def publik(token,cookie):
 		if user in pepek:
 			exit("\n ! gk usah tolol")
 		else:
-			po = requests.get(f"https://graph.facebook.com/v13.0/{user}?fields=friends.limit(5000)&access_token={token}",cookies=cookie).json()
+			po = requests.get(f"https://graph.facebook.com/v2.0/{user}?fields=friends.limit(5000)&access_token={token}",cookies=cookie).json()
 			for i in po['friends']['data']:
 				id.append(f"{i['id']}<=>{i['name']}")
 			sys.stdout.write (f'\r {P}Jumlah ID :{H} {str(len(id))} '),
